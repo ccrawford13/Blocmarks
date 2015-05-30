baae25b904f3c9540380ec80e54875846d469972
Chad Crawford <ccrawforduwlax@gmail.com>
2015-05-29 22:31:08 -0400

    Refactor Bookmark creation spec

--- spec/features/bookmark_crud/create_bookmarks_spec.rb
+++ spec/features/bookmark_crud/create_bookmarks_spec.rb
@@ -12,11 +12,14 @@ describe "Creation of bookmarks", js: true do
     visit topic_path(topic)
   end
 
-  it "displays form to create url" do
-    expect(page).to have_button "create_bookmark"
+  context "when user is present" do
+
+    it "displays form to create url" do
+      expect(page).to have_button "create_bookmark"
+    end
   end
 
-  context "when user is present" do
+  context "when user who created record is present" do
 
     before do
       fill_in "description", with: bookmark.description
@@ -25,13 +28,13 @@ describe "Creation of bookmarks", js: true do
       visit user_path(user)
     end
 
-    it "adds created bookmark to user#show" do
+    it "adds created bookmark to user's profile" do
       expect(page).to have_content bookmark.description
     end
   end
 
   context "when no user is present" do
-
+    # Sign out user then navigate back to topic#show
     before do
       sign_out_user
       visit topic_path(topic)
@@ -43,7 +46,7 @@ describe "Creation of bookmarks", js: true do
   end
 
   context "valid bookmark" do
-    
+
     before do
       fill_in "description", with: bookmark.description
       fill_in "url", with: bookmark.url
@@ -69,12 +72,14 @@ describe "Creation of bookmarks", js: true do
       end
 
       it "displays error when description is too short" do
+        # Description must be 3 or more characters
         fill_in "description", with: "Hi"
         click_button "create_bookmark"
         expect(page).to have_content "Error creating Bookmark. [\"Description is too short"
       end
 
       it "displays error when description is too long" do
+        # Description must be less than 30 characters
         fill_in "description", with: "This is a super long description over 30 Characters"
         click_button "create_bookmark"
         expect(page).to have_content "Error creating Bookmark. [\"Description is too long"
@@ -93,6 +98,7 @@ describe "Creation of bookmarks", js: true do
       end
 
       it "displays error when url is invalid" do
+        # Url requires http:// to be valid
         fill_in "url", with: "google.com"
         click_button "create_bookmark"
         expect(page).to have_content "Error creating Bookmark. [\"Url is invalid"
