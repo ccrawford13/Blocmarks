444c1cbd78a5715a4cb1a61614b12f81d2d7840a
Chad Crawford <ccrawforduwlax@gmail.com>
2015-05-29 23:55:46 -0400

    Refactor Bookmark & Topic Policy
    DRY up Bookmark & Topic Policy Pages to utilize Application Policy
    defaults

--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -1,18 +1,2 @@
 class BookmarkPolicy < ApplicationPolicy
-
-  def create?
-    user.present?
-  end
-
-  def edit?
-    user.present? &&  record.user == user
-  end
-
-  def update?
-    edit?
-  end
-
-  def destroy?
-    edit?
-  end
 end
