76e7a43f88bf0048f1d53a5c6b1e2e208edbad84
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-05 19:00:43 -0400

    Add Embedly Cards to User Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
     @users = UserDecorator.decorate_collection(User.all)
     @user = UserDecorator.find(params[:id])
     @topics = @user.topics.order(:created_at).page(params[:page]).per(5).decorate
-    @bookmarks = @user.bookmarks.order(:created_at).page(params[:page]).per(5).decorate
-    @likes = @user.likes.order(:created_at).page(params[:page]).per(5).decorate
+    @bookmarks = @user.bookmarks.order(:created_at).page(params[:page]).per(4).decorate
+    @likes = @user.likes.order(:created_at).page(params[:page]).per(4).decorate
   end
 
   def authorize_user
