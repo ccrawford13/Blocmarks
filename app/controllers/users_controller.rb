3ae92d8cc52fae4702d21087ce1ec222d7a71ad5
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-08 20:27:17 -0500

    Minor Code Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   decorates_assigned :user
   before_action :authorize_user
-  respond_to :html, :json
 
   def show
     @users = UserDecorator.decorate_collection(User.all)
@@ -9,6 +8,11 @@ class UsersController < ApplicationController
     @topics = @user.topics.order(:created_at).page(params[:page]).per(4).decorate
     @bookmarks = @user.bookmarks.order(:created_at).page(params[:page]).per(4).decorate
     @likes = @user.likes.order(:created_at).page(params[:page]).per(4).decorate
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def authorize_user
